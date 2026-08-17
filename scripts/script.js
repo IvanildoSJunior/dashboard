1cc8d81eb83fe4ad4b7acbcdd12dda28dabdec0e
ivanildo <ivanildojunior2319@gmail.com>
2026-08-17 10:10:54 -0300

    msg-boas-vindas

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,42 @@
-function atualizarRelogio() {
-    const agora = new Date();
-    const horario = agora.toLocaleTimeString('pt-BR');
-    document.getElementById('relogio').innerHTML = horario;
+const boasVindas = document.getElementById('msgEntrada');
+
+function obterSaudacao(hora) {
+    if(hora >=5 && hora < 12) {
+        return 'Bom dia';
+    } else if (hora >= 12 && hora < 18) {
+        return 'Boa tarde';
+    } else {
+        return 'Boa noite';
+    }
 }
 
-setInterval(atualizarRelogio, 1000);
+function atualizarRelogio() {
+    const agora = new Date();
+    const hora = agora.getHours();
+    const horario = agora.toLocaleTimeString('pt-BR');
+
+    document.getElementById('relogio').innerText = horario;
+
+    const nome = localStorage.getItem('nome');
+    if(nome) {
+        const saudacao = obterSaudacao(hora);
+        boasVindas.innerText = `${saudacao}, ${nome}!`
+    }
+}
+
+
+function pegarNome() {
+    let nome = localStorage.getItem('nome')
+
+    if(!nome) {
+        nome = prompt('Qual é o seu nome?');
+        while (nome === null || nome.trim() === "") {
+            nome = prompt("Por favor digite algo! Qual é o seu nome?");
+        }
+        nome.trim();
+        localStorage.setItem('nome', nome);
+    }
+}
+pegarNome();
 atualizarRelogio();
+setInterval(atualizarRelogio, 1000);
